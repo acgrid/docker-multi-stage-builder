f765c3b556a622ba44bde7e80d31e66e27591d72
wudi <acgrid@gmail.com>
2022-02-17 23:03:36 +0800

    Update to latest CentOS 8

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 source /etc/profile.d/build-env.sh
 
 CMAKE_VERSION=${CMAKE_VERSION}
-LIBZIP_VERSION=${LIBZIP_VERSION}
 BUILD_ASSETS_DIR=${BUILD_ASSETS_DIR}
 BUILD_PREFIX_DIR=${BUILD_PREFIX_DIR}
 
 set -e
+# CentOS is dead
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 yum update -y && yum install -y epel-release
-yum install -y git gcc gcc-c++ make automake autoconf bison perl file tar re2c libtool wget bzip2 mlocate which
+yum install -y git gcc gcc-c++ make automake autoconf bison perl libtool wget bzip2 mlocate
 
 wget https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-Linux-x86_64.sh
 bash cmake-${CMAKE_VERSION}-Linux-x86_64.sh --skip-license --prefix=/usr && rm -f cmake-${CMAKE_VERSION}-Linux-x86_64.sh
